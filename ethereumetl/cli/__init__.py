e4c681dfac511200ecd39b812f8f133de3a6f1a0
Dev0 <dev0@syspulse.io>
2023-02-21 15:22:35 +0200

    Update version

--- ethereumetl/cli/__init__.py
+++ ethereumetl/cli/__init__.py
@@ -48,7 +48,7 @@ from ethereumetl.cli.stream import stream
 
 
 @click.group()
-@click.version_option(version='2.1.2.hknio')
+@click.version_option(version='2.1.2.1')
 @click.pass_context
 def cli(ctx):
     pass
